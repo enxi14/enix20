7df52e02f4f5414da387c4b1d8f87c76e013af58
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-10-14 14:02:09 -0400

    commit

--- script.js
+++ script.js
@@ -1,66 +1,57 @@
 $(document).ready(function() {
-    var currentInput = '';
-    var currentOperator = '';
-    var currentTotal = '';
-
-    // Click event handlers for number buttons
-    $('.button').not('#calculate').click(function() {
-        currentInput += $(this).text();
-        $('#display').val(currentInput);
-    });
-
-    // Click event handler for operator buttons
-    $('.button').not('#calculate').click(function() {
-        if (currentTotal === '') {
-            currentTotal = currentInput;
-            currentInput = '';
-        } else {
-            currentTotal = calculate(currentTotal, currentInput, currentOperator);
-            currentInput = '';
-        }
-        currentOperator = $(this).text();
-        $('#display').val(currentTotal);
-    });
-
-    // Click event handler for the equals button
-    $('#calculate').click(function() {
-        if (currentOperator !== '') {
-            currentTotal = calculate(currentTotal, currentInput, currentOperator);
-            $('#display').val(currentTotal);
-            currentInput = '';
-            currentOperator = '';
-        }
-    });
-
-    // Click event handler for the clear button
-    $('#clear').click(function() {
-        currentInput = '';
-        currentOperator = '';
-        currentTotal = '';
-        $('#display').val('');
-    });
-
-    // Click event handler for the backspace button
-    $('#backspace').click(function() {
-        currentInput = currentInput.slice(0, -1);
-        $('#display').val(currentInput);
-    });
-
-    // Calculation function
-    function calculate(num1, num2, operator) {
-        num1 = parseFloat(num1);
-        num2 = parseFloat(num2);
-        if (operator === '+') {
-            return num1 + num2;
-        } else if (operator === '-') {
-            return num1 - num2;
-        } else if (operator === '*') {
-            return num1 * num2;
-        } else if (operator === '/') {
-            if (num2 === 0) {
-                return 'Error';
+            let currentInput = '';
+            let previousInput = '';
+            let operator = '';
+            
+            $('button').click(function() {
+                const id = $(this).attr('id');
+                
+                if (!isNaN(id) || id === '.') {
+                    currentInput += id;
+                    $('#result').val(currentInput);
+                } else if (id === '+' || id === '-' || id === '*' || id === '/') {
+                    if (previousInput === '') {
+                        previousInput = currentInput;
+                        currentInput = '';
+                    } else {
+                        previousInput = operate(previousInput, currentInput, operator);
+                        currentInput = '';
+                    }
+                    operator = id;
+                    $('#result').val(previousInput);
+                } else if (id === '=') {
+                    if (previousInput !== '' && operator !== '') {
+                        currentInput = operate(previousInput, currentInput, operator);
+                        $('#result').val(currentInput);
+                        previousInput = '';
+                        operator = '';
+                    }
+                } else if (id === 'C') {
+                    currentInput = '';
+                    previousInput = '';
+                    operator = '';
+                    $('#result').val('0');
+                } else if (id === '←') {
+                    currentInput = currentInput.slice(0, -1);
+                    $('#result').val(currentInput);
+                } else if (id === '+/-') {
+                    currentInput = (parseFloat(currentInput) * -1).toString();
+                    $('#result').val(currentInput);
+                }
+            });
+            
+            function operate(a, b, op) {
+                a = parseFloat(a);
+                b = parseFloat(b);
+                switch (op) {
+                    case '+':
+                        return (a + b).toString();
+                    case '-':
+                        return (a - b).toString();
+                    case '*':
+                        return (a * b).toString();
+                    case '/':
+                        return (a / b).toString();
+                }
             }
-            return num1 / num2;
-        }
-    }
-});
+        });

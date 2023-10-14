cd5917429a63a3fa58e456a7ced4cdf13c22745e
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-10-14 14:05:19 -0400

    commit

--- script.js
+++ script.js
@@ -1,57 +1,30 @@
-$(document).ready(function() {
-            let currentInput = '';
-            let previousInput = '';
-            let operator = '';
-            
-            $('button').click(function() {
-                const id = $(this).attr('id');
-                
-                if (!isNaN(id) || id === '.') {
-                    currentInput += id;
-                    $('#result').val(currentInput);
-                } else if (id === '+' || id === '-' || id === '*' || id === '/') {
-                    if (previousInput === '') {
-                        previousInput = currentInput;
-                        currentInput = '';
-                    } else {
-                        previousInput = operate(previousInput, currentInput, operator);
-                        currentInput = '';
-                    }
-                    operator = id;
-                    $('#result').val(previousInput);
-                } else if (id === '=') {
-                    if (previousInput !== '' && operator !== '') {
-                        currentInput = operate(previousInput, currentInput, operator);
-                        $('#result').val(currentInput);
-                        previousInput = '';
-                        operator = '';
-                    }
-                } else if (id === 'C') {
-                    currentInput = '';
-                    previousInput = '';
-                    operator = '';
-                    $('#result').val('0');
-                } else if (id === '←') {
-                    currentInput = currentInput.slice(0, -1);
-                    $('#result').val(currentInput);
-                } else if (id === '+/-') {
-                    currentInput = (parseFloat(currentInput) * -1).toString();
-                    $('#result').val(currentInput);
-                }
-            });
-            
-            function operate(a, b, op) {
-                a = parseFloat(a);
-                b = parseFloat(b);
-                switch (op) {
-                    case '+':
-                        return (a + b).toString();
-                    case '-':
-                        return (a - b).toString();
-                    case '*':
-                        return (a * b).toString();
-                    case '/':
-                        return (a / b).toString();
-                }
-            }
-        });
+function appendToDisplay(value) {
+    $("#display").val($("#display").val() + value);
+}
+
+function clearDisplay() {
+    $("#display").val('');
+}
+
+function calculateResult() {
+    try {
+        var result = eval($("#display").val());
+        $("#display").val(result);
+    } catch (error) {
+        $("#display").val('Error');
+    }
+}
+
+$(document).ready(function () {
+    $(".number, .operator").click(function () {
+        appendToDisplay($(this).text());
+    });
+
+    $(".clear").click(function () {
+        clearDisplay();
+    });
+
+    $(".equals").click(function () {
+        calculateResult();
+    });
+});
